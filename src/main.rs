d79265cc681d8434f762e6c333cf72beff22950b
igram <igram.jp@gmail.com>
2020-02-14 17:19:12 +0900

    unit type

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
 fn main() {
+    // スカラ型
+    // ユニット (unit type)
+    // 関数を呼び出し (ないはずの) 戻り値に変数 ret を束縛する
+    let ret = hello();
+    // アサーションで ret の値がユニット値と等しいことを検査する
+    assert_eq!(ret, ());
+    // size_of::<型>() は，その型の値がメモリ上で占める大きさをバイト数で表す
+    assert_eq!(std::mem::size_of::<()>(), 0); // 0 バイト
+
+    // RPN
+    /*
     let exp = "6.1 5.2 4.3 * + 3.4 2.5 / 1.6 * -";
-
     let ans = rpn(exp);
-
     debug_assert_eq!("26.2840", format!("{:.4}", ans));
-
     println!("{} = {:.4}", exp, ans);
+    */
 }
 
+// スカラ型
+// ユニット
+fn hello() {
+    println!("hello");
+}
+
+// RPN
+/*
 fn rpn(exp: &str) -> f64 {
     let mut stack = Vec::new();
 
@@ -39,3 +56,4 @@ where
         panic!("Stack underflow");
     }
 }
+*/
