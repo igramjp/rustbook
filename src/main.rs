22dbb907aa45ce3c3b503504a21c0e2336e953af
igram <igram.jp@gmail.com>
2020-02-26 14:16:45 +0900

    slice type

--- src/main.rs
+++ src/main.rs
@@ -111,10 +111,96 @@ fn main() {
     }
     println!();
 
-    let mut array5 = [1; 50];
+    let mut array5 = [1; 32];
 
     // iter_mut() で要素が可変のイテレータを作成
     for n in array5.iter_mut() {
         *n *= 2;
     }
+    println!("{:?}", array5);
+
+    // スライス (slice type)
+    // 配列
+    let a6 = ['a', 'b', 'c', 'd']; // 参照元のデータ [char; 4] 型
+    println!("a6: {:?}", a6);
+
+    print_info("&a6[..]", &a6[..]); // &[char] 型．全要素のスライス
+    print_info("&a6", &a6); // 同上
+    print_info("&a6[1..3]", &a6[1..3]); // b と c を要素とする長さ 2 のスライス
+
+    // ベクタ
+    let v1 = vec!['e', 'f', 'g', 'h'];
+    println!("v1: {:?}", v1);
+
+    print_info("&v1[..]", &v1[..]); // &[char] 型．全要素のスライス
+    print_info("&v1", &v1); // 同上
+    print_info("&v1[1..3]", &v1[1..3]); // &[char]型．f と g を要素とする長さ 2 のスライス
+
+    // ミュータブルなスライス
+    let mut a7 = [5, 4, 3, 2]; // 配列．[i32; 4] 型
+    let s1 = &mut a7[1..3]; // 可変スライス．&mut[i32] 型
+    s1[0] = 6; // スライスの最初の要素を 6 にする
+    s1[1] *= 10; // 2 番目の要素を 10 倍する
+    s1.swap(0, 1); // 要素を交換する
+    assert_eq!(s1, [30, 6]); // スライスの内容を確認
+
+    // 参照元の配列の内容を確認
+    assert_eq!(a7, [5, 30, 6, 2]); // スライスを通じて配列の内容が変更された
+
+    // スライスに対する主な操作
+    let a8: [i32; 0] = [];
+    let s2 = &a8; // 不変のスライスを作成
+    assert!(s2.is_empty()); // 空のスライス
+    assert_eq!(s2.len(), 0); // 長さは 0
+    assert_eq!(s2.first(), None); // 最初の要素は存在しない
+
+    let a9 = ["zero", "one", "two", "three", "four"];
+    let s3 = &a9[1..4]; // 不変のスライスを作成
+    assert!(!s3.is_empty()); // 空ではない
+    assert_eq!(s3.len(), 3); // 長さは 3
+    assert_eq!(s3.first(), Some(&"one")); // 最初の要素
+
+    assert_eq!(s3[1], "two"); // 2 番目の要素
+                              // assert_eq!(s3[3], "?"); // 4 番目の要素．存在しないのでパニックする
+    assert_eq!(s3.get(1), Some(&"two")); // 2 番目の要素を得る別の方法
+    assert_eq!(s3.get(3), None); // 4 番目の要素．存在しないので None
+
+    assert!(s3.contains(&"two")); // "two" を要素に持つ
+    assert!(s3.starts_with(&["one", "two"])); // "one", "two" で始まる
+    assert!(s3.ends_with(&["two", "three"])); // "two", "three" で終わる
+
+    // 次は可変のスライスだけで可能な操作
+    let mut a10 = [6, 4, 2, 8, 0, 9, 4, 3, 7, 5, 1, 7];
+
+    // 一部の要素を昇順にソートする
+    // &mut を省略しても結果は同じ．型強制によって自動的にスライスが作られる
+    &mut a10[2..6].sort();
+    assert_eq!(&a10[2..6], &[0, 2, 8, 9]);
+
+    // スライスを 2 つの可変スライスへ分割する
+    let (s4a, s4b) = &mut a10.split_at_mut(5);
+
+    // 前半を逆順にする
+    s4a.reverse();
+    assert_eq!(s4a, &[8, 2, 0, 4, 6]);
+
+    // 後半を昇順にソートする
+    s4b.sort_unstable();
+    assert_eq!(s4b, &[1, 3, 4, 5, 7, 7, 9]);
+
+    // sort() と sort_unstable() の違い
+    // sort() は安定ソートなので同順なデータのソート前の順序がソート後も保存される
+    // sort_unstable() は安定ソートではないが，一般的に sort() より高速
+}
+
+// この関数は &[char] 型のスライスを引数に取り，その情報を表示する
+fn print_info(name: &str, sl: &[char]) {
+    println!(
+        "  {:9} - {}, {:?}, {:?}, {:?}",
+        name,
+        sl.len(),   // 長さ (バイト数) usize 型
+        sl.first(), // 最初の要素      Option<char> 型
+        sl[1],      // 2 番目の要素    char 型
+        sl.last()   // 最後の要素      Option<char> 型
+    );
 }
